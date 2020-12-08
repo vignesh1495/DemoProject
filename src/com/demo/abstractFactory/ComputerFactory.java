23374f503336b364e1797f00a22eaebe07362d5a
vignesh1495 <vigneshrajdev18@gmail.com>
2020-12-08 11:23:08 +0530

    'newbuildtoproject'

--- src/com/demo/abstractFactory/ComputerFactory.java
+++ src/com/demo/abstractFactory/ComputerFactory.java
@@ -4,6 +4,10 @@ import com.demo.beans.Computer;
 
 public class ComputerFactory {
 	
+	public void newMethod1() {
+		System.out.println("new line to push to git");
+	}
+	
 	public static Computer createComputer(ComputerAbstractFactory caf) {
 		return caf.createComputer();
 	}
